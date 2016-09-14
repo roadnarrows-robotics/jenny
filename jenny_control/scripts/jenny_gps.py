15a21f40d591c75e69a10834fdf33243e184fe2e
Bill Coon <william.g.coon@gmail.com>
2016-09-14 14:29:48 -0600

    Add GPS script

---
+++ jenny_control/scripts/jenny_gps.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import rospy
+import gps
+
+from std_msgs.msg import String
+from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatStatus
+
+def read_gps():
+    pub = rospy.Publisher('gpsRoverStatus', NavSatFix, queue_size=10)
+    rospy.init_node('jenny_gps', anonymous=True)
+    rate = rospy.Rate(20)
+    session = gps.gps("localhost", "2947")
+    session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+    navMsg = NavSatFix()
+    navStatus = NavSatStatus()
+    navStatus.service  = NavSatStatus.SERVICE_GPS
+    while not rospy.is_shutdown():
+        try:
+            navMsg.latitude = session.fix.latitude
+            navMsg.longitude = session.fix.longitude
+            if(session.status > 0):
+                navStatus.status = NavSatStatus.STATUS_FIX
+            else:
+                navStatus.status = NavSatStatus.STATUS_NO_FIX
+            navMsg.status = navStatus
+            rospy.loginfo(navMsg)
+            pub.publish(navMsg)
+            rate.sleep()
+        except AttributeError as e:
+            rospy.logwarn("Attrbute Error in GPS Data %s" % e)
+        except KeyError:
+            rospy.logwarn("Key Error in GPS Data")
+
+if __name__ == '__main__':
+    try:
+        read_gps()
+    except rospy.ROSInterruptException:
+        pass 
